9f79ca63dd9c1b5af2fae7fed2e5b0694d516a40
Isaac Pugh <me@isaacpugh.com>
2020-04-25 10:31:07 -0400

    added basic api stuff

--- main.go
+++ main.go
@@ -2,10 +2,30 @@ package main
 
 import (
     "github.com/gin-gonic/gin"
+    "fmt"
+    "log"
 )
+
+type Territory struct {
+    Territory string `json:"territory"`
+}
 
 func main() {
 	r := gin.Default()
     r.Static("/", "./assets")
+
+    r.POST("/pick", func(c *gin.Context) {
+        var territory Territory
+        err := c.BindJSON(&territory)
+        if err != nil {
+            log.Fatal(err)
+        }
+        fmt.Println(territory)
+
+        c.JSON(200, gin.H{
+            "success": true,
+        })
+    })
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
