b0df1e370303690fb063eaa8835d435e45bce935
Isaac Pugh <me@isaacpugh.com>
2020-04-25 09:13:51 -0400

    updated file structure

--- main.go
+++ main.go
@@ -2,14 +2,10 @@ package main
 
 import (
     "github.com/gin-gonic/gin"
-    "net/http"
 )
 
 func main() {
 	r := gin.Default()
-    r.LoadHTMLGlob("assets/*.html")
-    r.GET("/", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "index.html", nil)
-    })
+    r.Static("/", "./assets")
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
