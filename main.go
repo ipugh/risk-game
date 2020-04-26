b6fb32674d4b56980959b2e40ac4885c0fc453ba
Isaac Pugh <me@isaacpugh.com>
2020-04-25 23:28:04 -0400

    separated models into a new file

--- main.go
+++ main.go
@@ -3,28 +3,24 @@ package main
 import (
     "github.com/gin-gonic/gin"
     "fmt"
-    "log"
 )
-
-type Territory struct {
-    Territory string `json:"territory"`
-}
 
 func main() {
 	r := gin.Default()
     r.Static("/", "./assets")
 
     r.POST("/pick", func(c *gin.Context) {
-        var territory Territory
+        var territory Pick
         err := c.BindJSON(&territory)
         if err != nil {
-            log.Fatal(err)
-        }
-        fmt.Println(territory)
+            c.AbortWithError(400, err).SetType(gin.ErrorTypeBind)
+        } else {
+            fmt.Println(territory)
 
-        c.JSON(200, gin.H{
-            "success": true,
-        })
+            c.JSON(200, gin.H{
+                "success": true,
+            })
+        }
     })
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
